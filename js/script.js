aa4ef994490f390d04f1036f83a17011c590283a
Błażej Kazimierski <kazimierskiblazej@gmail.com>
2020-10-06 18:41:56 +0200

    add else if choices

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('zagrałem ' + playerMove + ' Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} 
+else if(randomNumber == 2){
+    computerMove = 'papier';
+}
+else{
+    computerMove = 'nożyce';
+}
+printMessage('Mój ruch to: ' + computerMove);
