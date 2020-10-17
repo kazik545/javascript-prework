e437c25755733a55075e7895ad13a7fb1419717c
Błażej Kazimierski <kazimierskiblazej@gmail.com>
2020-10-17 12:23:58 +0200

    fixed buttons and html

--- js/script.js
+++ js/script.js
@@ -60,10 +60,10 @@ function playGame(playerInput){
   displayResult(playerMove, computerMove)
 }
 document.getElementById('play-paper').addEventListener('click', function(){
-  playGame(0)
+  playGame(1)
 })
 document.getElementById('play-rock').addEventListener('click', function(){
-  playGame(1)
+  playGame(0)
 })
 document.getElementById('play-scissors').addEventListener('click', function(){
   playGame(2)
